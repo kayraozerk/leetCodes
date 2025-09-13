5a0ebae4d3b757f1948d6ea520b42c58d8310f1d
kayra ozerk <kayra.ozerk@sabanciuniv.edu>
2025-09-13 21:33:15 +0300

    longestCommon

---
+++ Strings/LongestCommonPrefix.cpp
@@ -0,0 +1,35 @@
+/*
+13.09.2025 Tankut Kayra Özerk
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        sort(strs.begin(),strs.end());
+        string str1 = strs[0];
+        string str2 = strs[strs.size() -1];
+        int idx = 0;
+
+        while(idx < str1.length() && idx < str2.length()){
+            if(str1[idx] == str2[idx]){
+                idx++;
+            }else{
+                break;
+            }
+        }
+        
+        string a = str1.substr(0,idx);
+         return a;
+        
+       
+    }
+};
